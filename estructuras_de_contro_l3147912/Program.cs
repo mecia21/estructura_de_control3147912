f7b4d9f81d823c497635d1a334998b86afc5db61
mecia21 <meciakeyner@gmail.com>
2025-10-16 16:30:43 -0500

    Subiendo proyecto completo

---
+++ estructuras_de_contro_l3147912/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace estructuras_de_contro_l3147912
+{
+    internal class Program
+    {
+        private const float V = 0.2f;
+
+        static void Main(string[] args)// punto de partida
+        {
+            //tipos de datos
+
+
+            //estructura de control
+            //Console.WriteLine("Ingrese su edad: .");
+            //int numero = int.Parse(Console.ReadLine()); //para leer la entrda de datos
+
+            //Console.WriteLine($"su edad es: "+numero);
+            //Console.WriteLine($"su edad es:  {numero}");
+
+
+            //Console.WriteLine("ingrese 3 notas");
+            //float n1 = int.Parse(Console.ReadLine());
+            //float n2 = int.Parse(Console.ReadLine());
+            //float n3 = int.Parse(Console.ReadLine());
+            //if (n1 < 0 | n1 >5 | n2 < 0 | n2 > 5 | n3 < 0 | n3 > 5) ;
+            //float promedioes = (n1 * 0.2f) + (n2 * 0.3f) + (n3 * 0.5f);
+            //if(promedioes>=3)
+            //{
+            //    Console.WriteLine("aprobado");
+            //}
+            //else
+            //{
+            //    Console.WriteLine("desaprobado");
+            //}
+
+            Console.WriteLine("ingrese el el precio del producto");
+            float product = int.Parse(Console.ReadLine());
+            if (product >= 100000)
+            {
+                Console.WriteLine($"se le aplica el 20% de descuento entonces es"prce)
+            }
+          
+        }
+    }
+}
